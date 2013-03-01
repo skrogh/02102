a44c26ef45cbe905be6edf3da6d335b99e75c549
Skrogh <s123369@student.dtu.dk>
2013-03-01 23:03:31 +0100

    Ryddet op ot tilfjet kommentarer

--- assignment_2/src/Opgave1.java
+++ assignment_2/src/Opgave1.java
@@ -2,165 +2,132 @@ import static java.lang.System.out;
 import java.util.Scanner;
 
 
+
+
+
 public class Opgave1 {
+	
+	/*
+	 * Literals have the following values:
+	 *	I = 1	 				*	V = 5
+	 *	X = 10	 				*	L = 50
+	 *  C = 100	 				*  D = 500
+	 *  M = 1000	 			*  V\u0305 = 5000
+	 *  X\u0305 = 10000	 		*  L\u0305 = 50000
+	 *  |I\u0305| = 100000	 	*  |V\u0305| = 500000
+	 *  |X\u0305| = 1000000	 	*  |L\u0305| = 5000000
+	 *  |C\u0305| = 10000000	*  |D\u0305| = 50000000
+	 *  |M\u0305| = 100000000
+	 */
+	
+	// these two constants store the literals in decade 1-9 and 1-8 respectively
+	public static final String[] LITERAL_ONES = 
+		{"I", "X", "C", "M", "X\u0305", "C\u0305", "|X\u0305|", "|C\u0305|", "|M\u0305|"};
+	public static final String[] LITERAL_FIVES = 
+		{"V", "L", "D", "V\u0305", "L\u0305", "|V\u0305|", "|L\u0305|", "|D\u0305|"};
 
 	public static void main(String[] args) {
+		
 		Scanner console = new Scanner(System.in); // open a scanner, scanning the console
-
-		out.println("Input any positive integer."); // print a message
-
-		String in; // temporary string storing the next input
-		in = console.nextLine(); // get the input
-		out.println();
-		if(isPosInt(in)) { // check if the entered input is really a number
-			String roman = arabToRoman(in);
-			out.println(roman); // print the number
+		
+		while(true) {
+			out.println("Input any positive integer. Type \"exit\" to end the program");
+	
+			String in  = console.nextLine(); // temporary string storing the input
 			
-					
-		} else {
-			out.println("Please enter an integer, " +
-					"that's a number without a decimal point," +
-					"larger than zero"); // throw error message to console
+			/* check if the entered input is really a number. We could use nextInt and handle exceptions, 
+			 * but this way we would not be able to: 1. look for "exit" 2. handle numbers larger than 2^31-1,
+			 * of cause this is a bit stupid when working with Roman numerals.
+			 */
+			if(isPosInt(in)) {  
+				out.println(arabToRoman(in)); // print the number	
+			} else if (in.equals("exit")) {
+				break; // escape the while-loop and continue.
+			} else {
+				out.println("Please enter an integer, " +
+						"that's a number without a decimal point," +
+						"larger than zero"); // throw error message to console
+			}
 		}
-
 		out.println("Done!");
 		
-		console.close(); // close the console-scanner
+		console.close();
 	}
 
 
-	public static String arabToRoman(long arabic) { // for ints
-		/*
-		 *	I = 1
-		 *	V = 5
-		 *	X = 10
-		 *	L = 50
-		 *  C = 100
-		 *  D = 500
-		 *  M = 1000
-		 *  From left to right:
-		 *  smaller in front of larger are subtracted from that;
-		 *  smaller (this also be a larger, that is subtracted from) after larger is added.
-		 *  only literals smaller than 1/5 of the next one can be in front of it.
-		 *  for now nothing greater than 3999 can be an input
-		 */
-		if(arabic>0){ // if in the allowed range
-			String str = String.valueOf(arabic); // break into string, to get length and individual literals
-			String roman = ""; // empty string for Roman number
-			for(int i = 0; i<str.length(); i++) {
-				int num = str.charAt(i) - 0x30; // get numbers from left; zero is at 0x30, nine is at 39
-				roman += arabLiteralToRoman(num, str.length()-i);
-			}
-			return roman;
-		} else {
+	public static String arabToRoman(long arabic) { // for numbers
+		// this functions simply converts the input to a string and calls the functions for strings
+		if(arabic>0) // if in the allowed range
+			return arabToRoman(String.valueOf(arabic)); // call the string Arab to Roman for the long
+		else
 			return "not in the correct range";
-		}
 	}
 	
 	public static String arabToRoman(String arabic) { // for strings
-		/*
-		 *	I = 1
-		 *	V = 5
-		 *	X = 10
-		 *	L = 50
-		 *  C = 100
-		 *  D = 500
-		 *  M = 1000
-		 *  From left to right:
-		 *  smaller in front of larger are subtracted from that;
-		 *  smaller (this also be a larger, that is subtracted from) after larger is added.
-		 *  only literals smaller than 1/5 of the next one can be in front of it.
-		 *  for now nothing greater than 3999 can be an input
-		 */
-		
-		String str = arabic; // break into string, to get length and individual literals
 		String roman = ""; // empty string for Roman number
-		for(int i = 0; i<str.length(); i++) {
-			int num = str.charAt(i) - 0x30; // get numbers from left; zero is at 0x30, nine is at 39
-			roman += arabLiteralToRoman(num, str.length()-i);
+		for(int i = 0; i<arabic.length(); i++) { // for each digit in the Arabic number convert it to the combination of Roman digits
+			int num = arabic.charAt(i) - 0x30; // get numbers from left; zero is at 0x30, nine is at 0x39
+			roman += arabLiteralToRoman(num, arabic.length()-i); // convert to Roman digits in the correct decade
 		}
 		return roman;
 	}
 
-
 	public static String arabLiteralToRoman(int num, int dec) {
-		// returns the Roman literal combination that equals the Arabic in decade dec
+		// returns the Roman literal combination that equals the Arabic in decade dec. if num is not in the range [1-9], "" is returned
 		String result = "";
-		switch (num) { // perhaps this can be optimized? but for what reason, this is fast in execution and writing
-		case 0:
-			// there are no zeros in Roman
-			break;
-		case 1:
-			result += getRomanLiterals(1, 1, dec); // get 1 ones in the correct decade;
-			break;
-		case 2:
-			result += getRomanLiterals(2, 1, dec); // get 2 ones in the correct decade;
-			break;	
-		case 3:
-			result += getRomanLiterals(3, 1, dec); // get 3 ones in the correct decade;
-			break;	
-		case 4:
-			result += getRomanLiterals(1, 1, dec) + getRomanLiterals(1, 5, dec); // get one, five in the correct decade;
-			break;	
-		case 5:
-			result += getRomanLiterals(1, 5, dec); // get five in the correct decade;
-			break;	
-		case 6:
-			result += getRomanLiterals(1, 5, dec) + getRomanLiterals(1, 1, dec); // get five, one in the correct decade;
-			break;	
-		case 7:
-			result += getRomanLiterals(1, 5, dec) + getRomanLiterals(2, 1, dec); // get five, one, one in the correct decade;
-			break;
-		case 8:
-			result += getRomanLiterals(1, 5, dec) + getRomanLiterals(3, 1, dec); // get five, one, one, one in the correct decade;
-			break;	
-		case 9:
-			result += getRomanLiterals(1, 1, dec) + getRomanLiterals(1, 1, dec+1); // get one, ten in the correct decade;
-			break;	
+		if (dec>8) { // if we can no longer write numbers as combinations of literals, switch to base 1 and the correct amount of 100000000
+			for(int i = 0; i<Math.pow(10, dec-9);i++) { // create 10^(dec-9)
+				result += getRomanLiterals(num, 1, 9); // times num;
+			}
+		} else { // get the combination of Roman literals giving num in decade dec
+			switch (num) {
+			case 0:
+				// there are no zeros in Roman
+				break;
+			case 1:
+				result += getRomanLiterals(1, 1, dec); // get 1 ones in the correct decade;
+				break;
+			case 2:
+				result += getRomanLiterals(2, 1, dec); // get 2 ones in the correct decade;
+				break;	
+			case 3:
+				result += getRomanLiterals(3, 1, dec); // get 3 ones in the correct decade;
+				break;	
+			case 4:
+				result += getRomanLiterals(1, 1, dec) + getRomanLiterals(1, 5, dec); // get one, five in the correct decade;
+				break;	
+			case 5:
+				result += getRomanLiterals(1, 5, dec); // get five in the correct decade;
+				break;	
+			case 6:
+				result += getRomanLiterals(1, 5, dec) + getRomanLiterals(1, 1, dec); // get five, one in the correct decade;
+				break;	
+			case 7:
+				result += getRomanLiterals(1, 5, dec) + getRomanLiterals(2, 1, dec); // get five, one, one in the correct decade;
+				break;
+			case 8:
+				result += getRomanLiterals(1, 5, dec) + getRomanLiterals(3, 1, dec); // get five, one, one, one in the correct decade;
+				break;	
+			case 9:
+				result += getRomanLiterals(1, 1, dec) + getRomanLiterals(1, 1, dec+1); // get one, ten in the correct decade;
+				break;	
+			}
 		}
 		return result;
 	}
 
-
 	public static String getRomanLiterals(int i, int num, int dec) {
 		// get the Roman literal for num in decade dec, i times
 		String literal = "";
+		
 		if(num==1) {
-			// choose 1, 10, 100, 1000 (1 is first decade)
-			switch(dec) {
-			case 1:
-				literal = "I";
-				break;
-			case 2:
-				literal = "X";
-				break;
-			case 3:
-				literal = "C";
-				break;
-			case 4:
-				literal = "M";
-				break;
-			default:
-				if(dec>4)
-					literal = generateRomanOne(dec); // generate with |))
-			}
+			// choose 1, 10, 100, 1000 ... etc (1 is first decade, 10 second etc.)
+			literal = LITERAL_ONES[dec-1];
 		} else {
-			// choose 5, 50, 500 (5 is first decade)
-			switch(dec) {
-			case 1:
-				literal = "V";
-				break;
-			case 2:
-				literal = "L";
-				break;
-			case 3:
-				literal = "D";
-				break;
-			default:
-				if(dec>3)
-					literal = generateRomnfive(dec); // generate with ((|))
-			}
+			// choose 5, 50, 500 ... etc (5 is first decade, 50 second etc.)
+			literal = LITERAL_FIVES[dec-1];
 		}
+		
 		// add i of the literals) 
 		String result = "";
 		for(int j = 0; j<i; j++) {
@@ -170,34 +137,13 @@ public class Opgave1 {
 	}
 
 
-	public static String generateRomnfive(int dec) {
-		// generate fives |) = 50, |)) = 500 etc.
-		String result = "|";
-		for(int i=0; i<dec-2; i++) 
-			result += "Ɔ";
-		
-		return result;
-	}
-
-
-	public static String generateRomanOne(int dec) {
-		// generate ones (|) = 1000, ((|)) = 10000 etc.
-		String result = "";
-		for(int i=0; i<dec-3; i++) 
-			result += "C";
-		result += "|";
-		for(int i=0; i<dec-3; i++) 
-			result += "Ɔ";
-		return result;
-	}
-
-
+	// did not get used in the end
 	public static boolean isInt(String in) {
 		boolean result = in.length()>0; // if no length; it's not an int.
 
 		for(int i = 0; (i<in.length())&&(result); i++) { // break if we hit a non-number 
 			char c = in.charAt(i);
-			// set to false, if any char is NOT an int; the first char may be a minus
+			// set to false, if any char is NOT a number; the first char may be a minus
 			result &= (c=='0')||(c=='1')
 					||(c=='2')||(c=='3')
 					||(c=='4')||(c=='5')
@@ -213,7 +159,7 @@ public class Opgave1 {
 
 		for(int i = 0; (i<in.length())&&(result); i++) { // break if we hit a non-number 
 			char c = in.charAt(i);
-			// set to false, if any char is NOT an int
+			// set to false, if any char is NOT a number
 			result &= ((c=='0')&&(i!=0)) // not zero
 					||(c=='1')
 					||(c=='2')||(c=='3')
