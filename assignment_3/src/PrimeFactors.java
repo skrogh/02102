5d360bcba4cb15470f723ce3de1b1a82a0050153
overlord <theexplosivesheep@gmail.com>
2013-03-17 17:55:53 +0100

    add comments

--- assignment_3/src/PrimeFactors.java
+++ assignment_3/src/PrimeFactors.java
@@ -1,4 +1,9 @@
-
+/*===================================================
+ *PrimeFactors
+ * Computes the prime factors of a number inputted
+ * by a user and outputs them as a list.
+ *===================================================
+ */
 import java.util.*;
 
 public class PrimeFactors {
@@ -19,8 +24,14 @@ public class PrimeFactors {
 		}
 	}
 	
-	
+    /*================================================================
+     * primeFactors()
+     * Attemps to find the prime factors of a number by iterative
+     * division
+     * ==============================================================
+     */
 	public static ArrayList<Long> primeFactors( long n ) {
+
 		ArrayList<Long> factorList = new ArrayList<Long>();
 		
 		for (long i = 2; i < ( (long) Math.sqrt( n ) + 1 ); i++ ) {
@@ -37,6 +48,12 @@ public class PrimeFactors {
 		return factorList;
 	}
 	
+    /*===============================================================
+     * getInput()
+     *  prompts the user for a positive integer larger than 1
+     *  for factorization. Ends the program if the input is "exit"
+     *  ============================================================
+     */
 	public static long getInput() {
 		System.out.println( "Enter a positive integer greater than 1 to factorize" +
 				", exit to termite: " );
