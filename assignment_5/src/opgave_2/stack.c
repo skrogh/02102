03976c170a19e7396610d30e7e4b6c74f2273e66
Overlord <theexplosivesheep@gmail.com>
2013-05-11 15:58:46 +0200

    add opgave 2 tests

--- assignment_5/src/opgave_2/stack.c
+++ assignment_5/src/opgave_2/stack.c
@@ -1,5 +1,9 @@
+//=========================================
 // stack.c
-
+// Implements a simple integer stack
+// Stack maximum size is environment
+// specific.
+//=========================================
 #include <stdlib.h>
 #include <stdio.h>
 #include "stack.h"
@@ -14,9 +18,21 @@ int pop( stack_t * stack_p ) {
     return stack_p -> array[ --stack_p -> size ];
 }
 
+//=======================================
+// push
+// Pushes an integer to the stack, also
+// handles stack resizing.
+//=======================================
 void push( stack_t * stack_p, int value ) {
     if ( stack_p -> size >= stack_p -> capacity ) {
-        stack_p -> array = realloc( stack_p -> array, 2 * stack_p -> capacity * sizeof ( int ) );
+        stack_t * tmp = realloc( stack_p -> array, 2 * stack_p -> capacity * sizeof ( int ) );
+
+        if ( tmp != NULL )
+            stack_p -> array = tmp;
+        else {
+            printf( "Integer stack too large, halting process" );
+            exit(1);
+        }
 
         stack_p -> capacity *= 2;
         stack_p -> array[ stack_p -> size++ ] = value;
@@ -25,10 +41,19 @@ void push( stack_t * stack_p, int value ) {
     }
 }
 
+//============================
+// top
+// Peeks at the stack.
+//============================
 int top( stack_t * stack_p ) {
     return stack_p -> array[ stack_p -> size - 1 ];
 }
 
+//========================================
+// newStack
+// Returns pointer to a new stack with
+// initial capacity 1.
+//=======================================
 stack_t * newStack( void ) {
     stack_t * stack_p = malloc( sizeof ( stack_t ) );
     stack_p -> capacity = 1;
@@ -38,6 +63,11 @@ stack_t * newStack( void ) {
     return stack_p;
 }
 
+//==============================
+// empty
+// Returns 1 if stack_p is empty
+// 0 otherwise
+//==============================
 int empty( stack_t * stack_p ) {
     return ( stack_p -> size ) <= 0;
 }
--- assignment_5/src/opgave_2/stackMain.c
+++ assignment_5/src/opgave_2/stackMain.c
@@ -1,27 +1,45 @@
 // stackMain.c
 
 #include <stdio.h>
+#include <time.h>
 #include "stack.h"
 
-/*
-
-Udskrift:
-
-popped: 4444
-popped: 99
-popped: 123
-
-*/
-
 int main() {
+  //create stack for testing
   stack_t * myStack = newStack();
-  push(myStack, 123);
-  push(myStack, 99);
-  push(myStack, 4444);
+  //push number that will cause overflow
+  push( myStack, 1313131313131 );
+  //testing vars
+  int j = 0;
+  srand( time( NULL ) );
+
+  //push 10 random numbers
+  for( j = 0; j < 10; j++ ) {
+    push( myStack, rand() % 1337 );
+  }
+
+  //pop the random numbers and continue popping
+  // even though the stack is empty
+  for( j = 0; j < 20; j++ ) {
+    printf( "popped %d\n", pop(myStack) );
+  }
+  free( myStack );
+  // reset the stack
+  myStack = newStack();
+  //test peeking at the stack
+  push( myStack, 42 );
+  printf( "peeking: %d \n", top( myStack ) );
+
+  //Make the stack too large
   while (!empty(myStack)) {
+    int i = 0;
+    for( i=0; i< 13421772; i++ ) {
+        push(myStack, i);
+    }
+
     int value;
     value = pop(myStack);
-    printf("popped: %d\n", value );
+    printf("popped: %d  %d  %d \n", value, myStack -> size, myStack -> capacity );
   }
   return 0;
 }
